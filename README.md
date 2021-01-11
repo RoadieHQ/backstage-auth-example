58a695437d9cdf2134d0de137ebc4908a81c2b42
Jussi Hallila <jussi@hallila.com>
2021-01-11 08:37:19 +0100

    Add Github auth demo.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Demo Backstage application with Auth providers
 
+Github authentication demo application.
+
+Set environment variables:
+ * AUTH_GITHUB_CLIENT_ID
+ * AUTH_GITHUB_CLIENT_SECRET
 
 See documentation in Backstage documentation page in: https://backstage.io/docs/tutorials/quickstart-app-auth
--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -6,10 +6,6 @@ import {
   SidebarPage,
   createRouteRef,
   githubAuthApiRef,
-  googleAuthApiRef,
-  gitlabAuthApiRef,
-  auth0AuthApiRef,
-  microsoftAuthApiRef,
   SignInPage
 } from '@backstage/core';
 import { apis } from './apis';
@@ -30,27 +26,7 @@ const authProviders = [
     title: 'GitHub',
     message: 'Login via Github',
     apiRef: githubAuthApiRef,
-  },  {
-    id: 'google-auth-provider',
-    title: 'Google',
-    message: 'Login via Google',
-    apiRef: googleAuthApiRef,
-  },  {
-    id: 'gitlab-auth-provider',
-    title: 'Gitlab',
-    message: 'Login via Gitlab',
-    apiRef: gitlabAuthApiRef,
-  },  {
-    id: 'auth0-auth-provider',
-    title: 'Auth0',
-    message: 'Login via Auth0',
-    apiRef: auth0AuthApiRef,
-  },{
-    id: 'microsoft-auth-provider',
-    title: 'Microsoft',
-    message: 'Login via Microsoft',
-    apiRef: microsoftAuthApiRef,
-  },
+  }
 ]
 
 
